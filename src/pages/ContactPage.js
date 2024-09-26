7ce02f1645dedc196342335b1082a9937236399b
Akhilesh Yadav <ydvakhilesh16@gmail.com>
2024-09-27 02:58:14 +0530

    add some responsive in form

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoIosSend } from "react-icons/io";
+import { FaCircleArrowDown } from "react-icons/fa6";
 
 function ContactPage(){
     useEffect(()=>{
@@ -8,11 +9,12 @@ function ContactPage(){
     },[])
     return(
         <div className="h-fit text-slate-400 w-screen my-20">
-            <div className="w-full sm:w-2/3 h-full mx-auto">
-                <form className='flex flex-col gap-12 w-full mx-3 mr-3 sm:mx-auto h-full py-10 px-20 shadow-[0_0_5px_rgba(100,116,139,0.3)] border border-slate-500 rounded-xl '>
+            <div className="w-full sm:w-2/3 h-full sm:mx-auto">
+                <h2 className="w-full text-center font-serif font-semibold text-3xl sm:text-5xl my-5 whitespace-nowrap">Reach out to me</h2>
+                <form className='flex flex-col gap-10 sm:gap-12 w-11/12 mx-auto h-full py-2 sm:py-10 sm:px-20 pr-2 shadow-[0_0_5px_rgba(100,116,139,0.3)] border-2 border-slate-500 rounded-xl'>
                     <div className='flex gap-2 w-full items-center'>
                         <label htmlfor="firstname" className="text-lg font-semibold whitespace-nowrap">First Name:</label>
-                        <input type="text" placeholder=" Enter first name" name="firstname" id="firstname " className="flex-grow h-12 outline-none w-full bg-slate-500 bg-opacity-20 mx-auto shadow-[0_0_5px_rgba(100,116,139,0.3)] border border-slate-500 rounded-xl"/>
+                        <input type="text" placeholder=" Enter first name" name="firstname" id="firstname " className="flex-grow h-12 outline-none w-full bg-slate-500 bg-opacity-20 shadow-[0_0_5px_rgba(100,116,139,0.3)] border border-slate-500 rounded-xl"/>
                     </div>
                     <div className='flex gap-2 w-full items-center'>
                         <label htmlfor="lastname" className="text-lg font-semibold whitespace-nowrap">Last Name : </label>
@@ -33,6 +35,8 @@ function ContactPage(){
                     <button className="w-32 mx-auto text-lg font-semibold h-14 text-slate-400 rounded-lg shadow-[0_0_5px_rgba(100,116,139,0.3)] border border-slate-500 hover:text-slate-500 transition-colors duration-300">Send <IoIosSend className="inline h-8 w-8 my-auto" /></button>
                     
                 </form>
+                <h2 className="w-full text-center font-serif font-semibold text-xl sm:text-3xl md:text-5xl mt-10 ">Let's connect through Social Media <FaCircleArrowDown className="mx-auto text-5xl mt-8"/></h2>
+
             </div>
             
         </div>
