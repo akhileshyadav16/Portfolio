08069ee8c9d68cee6c8b3514420d0f8d8ad05f64
Akhilesh Yadav <ydvakhilesh16@gmail.com>
2024-09-27 14:23:01 +0530

    animation2

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
+import { IoIosClock } from "react-icons/io";
 
 function AboutPage(){
     useEffect(()=>{
         window.scrollTo(0,0);
     },[])
     return(
-        <div className="h-screen text-slate-400 flex flex-col items-center justify-center font-semibold text-3xl font-mono">
+        <div className="h-screen text-slate-400 flex flex-col items-center justify-center font-semibold text-3xl gap-4 font-mono">
+            <IoIosClock className="animate-ping"/>
             <p>About Page</p>
             <div>Upcoming soon...</div>
         </div>
